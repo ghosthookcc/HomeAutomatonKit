9556b0128e4f8424210264c5e8b77a179e2169dd
ghosthookcc <kasperthornsten@gmail.com>
2025-11-25 19:07:14 +0100

    Implement boilerplate for internal API structure

--- app/HomeKit/dashboard/lib/dashboard/internal_api/data_analysis.ex
+++ app/HomeKit/dashboard/lib/dashboard/internal_api/data_analysis.ex
@@ -1,33 +1,6 @@
 defmodule Dashboard.InternalApi.DataAnalysis do 
-  use DashboardWeb, :html
-  use Phoenix.LiveComponent
-
-  @refresh_rate :timer.seconds(10)
-
   def generateDataPlot() do
-    # Implement later
-  end
-
-  def mount(_params, _session, socket) do
-    if connected?(socket) do
-      schedule_plot_update()
-    end
-
-    {:ok, assign(socket, time: DateTime.utc_now())}
-  end
-
-  def handle_plot_update(:tick, socket) do
-    schedule_plot_update()
-    {:noreply, assign(socket, time: DateTime.utc_now())}
-  end
-
-  defp schedule_plot_update() do
-    Process.send_after(self(), :tick, @refresh_rate)
-  end
-
-  def plot(assigns) do
-    ~H"""
-    <img src="#"/>
-    """
+    now = DateTime.utc_now()
+    %{ timestamp: now, url: "#" }
   end
 end
--- app/HomeKit/dashboard/lib/dashboard_web/live/dashboard_live.ex
+++ app/HomeKit/dashboard/lib/dashboard_web/live/dashboard_live.ex
@@ -1,5 +1,8 @@
 defmodule DashboardWeb.DashboardLive do
   use DashboardWeb, :live_view
 
+  alias Dashboard.InternalApi.DataAnalysis
+
   import Dashboard.InternalApi.DataAnalysis
+  import DashboardWeb.DataAnalysisComponents
 end
