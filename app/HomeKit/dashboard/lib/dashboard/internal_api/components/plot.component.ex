9fbb70deed85eeac4e1804cf36fdf6ab24cf7d5e
ghosthookcc <kasperthornsten@gmail.com>
2025-12-03 18:48:28 +0100

    Implement data_analysis API base

--- app/HomeKit/dashboard/lib/dashboard/internal_api/components/plot.component.ex
+++ app/HomeKit/dashboard/lib/dashboard/internal_api/components/plot.component.ex
@@ -1,9 +1,7 @@
 defmodule DashboardWeb.DataAnalysisComponents do
   use Phoenix.Component
-  use Phoenix.LiveView
 
   alias Dashboard.InternalApi.DataAnalysis
-  import Phoenix.LiveView
   import Phoenix.Component
 
   def plot(assigns) do
@@ -13,31 +11,5 @@ defmodule DashboardWeb.DataAnalysisComponents do
       <p>Updated: <%= @plot.timestamp %></p>
     </div>
     """
-  end
-
-  defmodule AutoPlotLive do
-    use Phoenix.LiveView
-
-    @tick :timer.seconds(5)
-
-    def mount(_params, _session, socket) do
-      if connected?(socket), do: schedule_tick()
-
-      plot = DataAnalysis.generateDataPlot()
-      {:ok, assign(socket, :plot, plot)}
-    end
-
-    def handle_tick(:tick, socket) do
-      schedule_tick()
-      {:noreply, assign(socket, :plot, DataAnalysis.generateDataPlot())}
-    end
-
-    defp schedule_tick, do: Process.send_after(self(), :tick, @tick)
-
-    def render(assigns) do
-      ~H"""
-      <DashboardWeb.DataAnalysisComponents.plot plot={@plot} />
-      """
-    end
   end
 end
